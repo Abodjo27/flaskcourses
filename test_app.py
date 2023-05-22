aee67aadc51727b69fede326870ccda8702f9956
Akpagni5547 <85566432+Akpagni5547@users.noreply.github.com>
2023-05-22 17:30:29 +0000

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,1 +1,76 @@
+from flask import Flask
+import pytest
 
+from app import app
+
+
+
+@pytest.fixture
+def client():
+    app.config["TESTING"] = True
+    with app.test_client() as client:
+        yield client
+
+
+def test_home_page(client):
+    response = client.get("/")
+    assert response.status_code == 200
+
+
+def test_create_todos(client):
+    response = client.post(
+        "/todos",
+        json={
+            "title": "Test Todo",
+            "description": "This is a test todo",
+            "status": 0,
+            "due": "2023-01-01",
+        },
+    )
+    assert response.status_code == 302  # Redirect
+    
+def test_get_todos(client):
+    response = client.get("/todos")
+    assert response.status_code == 200
+    
+def test_json_saver():
+    # Créer une instance de JsonSaver avec un fichier JSON de test
+    json_saver = JsonSaver("app_data.json")
+
+    # Ajouter des données de test dans le fichier JSON
+    data1 = {
+        "id": str(uuid.uuid4()),
+        "title": "Test Todo 1",
+        "description": "This is test todo 1",
+        "status": 0,
+        "due": "2023-01-01",
+    }
+    json_saver.add(data1["id"], data1)
+
+    data2 = {
+        "id": str(uuid.uuid4()),
+        "title": "Test Todo 2",
+        "description": "This is test todo 2",
+        "status": 1,
+        "due": "2023-01-02",
+    }
+    json_saver.add(data2["id"], data2)
+
+    # Utiliser JsonSaver pour lire les données du fichier JSON
+    todos = json_saver.find_all()
+
+    # Vérifier que les données sont correctement lues
+    assert len(todos) == 2
+    assert todos[0]["title"] == "Test Todo 1"
+    assert todos[0]["description"] == "This is test todo 1"
+    assert todos[0]["status"] == 0
+    assert todos[0]["due"] == "2023-01-01"
+    assert todos[1]["title"] == "Test Todo 2"
+    assert todos[1]["description"] == "This is test todo 2"
+    assert todos[1]["status"] == 1
+    assert todos[1]["due"] == "2023-01-02"
+
+
+
+if __name__ == "__main__":
+    pytest.main()
