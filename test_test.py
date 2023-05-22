55d8d8b340a68357d66049497c5c293b0678ec6e
Akpagni5547 <85566432+Akpagni5547@users.noreply.github.com>
2023-05-22 16:31:27 +0000

    Update test_test.py

--- test_test.py
+++ test_test.py
@@ -1,10 +1,41 @@
-from test import create, view
+from flask import Flask
+import pytest
 
-def test_create():
-    assert create() == "ajouté"
-    assert len(view()) == 1
+from app import app
 
-def test_view():
-    todos = view()
-    assert len(todos) == 1
-    assert todos[0] == "text"
+
+@pytest.fixture
+def client():
+    app.config["TESTING"] = True
+    with app.test_client() as client:
+        yield client
+
+
+def test_home_page(client):
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b"todoapp" in response.data
+
+
+def test_create_todos(client):
+    response = client.post(
+        "/todos",
+        json={
+            "title": "Test Todo",
+            "description": "This is a test todo",
+            "status": 0,
+            "due": "2023-01-01",
+        },
+    )
+    assert response.status_code == 302  # Redirect
+    assert response.headers["Location"] == "http://localhost/todos"
+
+
+def test_get_todos(client):
+    response = client.get("/todos")
+    assert response.status_code == 200
+    assert b"Test Todo" in response.data
+
+
+if __name__ == "__main__":
+    pytest.main()
